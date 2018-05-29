cc33ac41b72fb7f8c99e891965cbf9a2b5765ef7
Matt Greff <greff.matt@icloud.com>
2018-05-29 20:19:13 +0000

    adding knex implementations of editing a table

--- famous.js
+++ famous.js
@@ -1,8 +1,51 @@
-nameQuery = process.argv.slice(2);
-console.log(nameQuery)
-const pg = require("pg");
+famousPersonToAdd = process.argv.slice(2);
 const settings = require("./settings"); // settings.json
 
+var knex = require('knex')({
+  client: 'pg',
+  connection: {
+    user     : settings.user,
+    password : settings.password,
+    database : settings.database,
+    host     : settings.hostname,
+    ssl      : settings.ssl
+  }
+});
+
+// these next two functions output the same value, one uses raw while the first uses knex syntax and has error handing
+const allFamous = () => {
+  knex.select('*')
+    .from('famous_people')
+    .asCallback( (err, rows) => {
+      if (err) return console.error(err);
+      rows.forEach((person) => {
+        console.log(`${person.first_name} ${person.last_name} born ${(person.birthdate)}`)
+      });
+    });
+}
+
+const addPerson = () => {
+  knex('famous_people')
+  .insert([{
+    first_name : famousPersonToAdd[0],
+    last_name  : famousPersonToAdd[1],
+    birthdate  : famousPersonToAdd[2]
+  }])
+  .then(() => {
+    console.log('success!');
+    allFamous();  
+  })
+}
+
+famousPersonToAdd[2] ? addPerson() : console.log('incorrect parameters...');
+
+/* 
+knex.raw('select * from famous_people')
+  .then((result) => {
+    result.rows.forEach((name) => {
+      console.log(`${name.first_name} ${name.last_name} born ${(name.birthdate)}`)
+    });
+  });
 const client = new pg.Client({
   user     : settings.user,
   password : settings.password,
@@ -25,4 +68,4 @@ client.connect((err) => {
     }); //output: queried name
     client.end();
   });
-});
+}); */
